2eb38bc90437ae3e27f43fbe206051e9a5e4f37d
tohshima <tetsu.ohshima@gmail.com>
2024-03-03 16:08:34 +0900

    Completed project04 asm implementation.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,65 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// count: number of words of screen
+// pixel: black(0xFFFF) or white(0) write value
+// current: current pointer to word address of screen
+
+(LOOP)
+
+// write count 512x256/16
+// initialize count
+@8192
+D=A
+@count
+M=D
+
+// check key pushed
+@KBD
+D=M
+
+// black or white value. it is set to 0xFFFFFFFF if key pressed
+@pixel
+M=0
+
+@CHECKED
+D;JEQ
+
+@pixel
+M=!M
+
+(CHECKED)
+
+// initialize current pointer to the head of screen (0x4000)
+@SCREEN
+D=A
+
+@current
+M=D     
+
+(WRITELOOP)
+// load count to D
+@count
+D=M
+// count--
+M=D-1
+// exit if count=0 (counte refers to that of before decrement)
+@WRITEEND
+D;JEQ
+
+// write pixels *current=black
+@pixel
+D=M
+@current
+A=M
+M=D
+// current++
+@current
+M=M+1
+
+@WRITELOOP
+0;JMP
+
+(WRITEEND)
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// set R2 to 0
+@2
+M=0
+
+// load R1 to D
+@1
+D=M
+
+(LOOP)
+// exit if R1=0
+@END
+D;JEQ 
+// load R0 to D
+@0
+D=M
+// R2 = R2+D
+@2
+M=M+D
+// load R1 to D
+@1
+D=M
+// MD = D-1
+MD=D-1
+// Re-loop
+@LOOP
+0;JMP
+(END)
+
